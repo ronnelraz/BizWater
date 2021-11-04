7b91796a8f1b037fd6cc088d76f8eb90722e1f7a
ronnelraz <68990196+ronnelraz@users.noreply.github.com>
2021-11-04 20:36:48 +0800

    dana

--- app/src/main/java/com/example/bizwater/connection/config.java
+++ app/src/main/java/com/example/bizwater/connection/config.java
@@ -1,6 +1,6 @@
 package com.example.bizwater.connection;
 
 public class config {
-    public static final String URL = "https://empty-chicken-76.loca.lt/systemwatech/android/v1/";
-    public static final String IMGURL = "https://empty-chicken-76.loca.lt/systemwatech/cvms/";
+    public static final String URL = "http://172.20.160.1/systemwatech/Android/v1/";
+    public static final String IMGURL = "http://172.20.160.1/systemwatech/cvms/";
 }
